5be459c9849c5e68748e519d667bf7825eb09e8d
=mux57 <mukesh.gupta@knolskape.com>
2017-09-03 23:13:49 +0530

    nested component ep7

--- src/App.js
+++ src/App.js
@@ -1,28 +1,39 @@
 import React from 'react';
 class App extends React.Component{
-    constructor(){
-        super();
-        this.state = {
-            txt: "this is the value from app.js"
-        }
+
+    render() {
+        return <Button> yo <Heart /> </Button>
     }
-    update( e){
-        this.setState({txt: e.target.value});
-    }
-    render(){
-        return (
-            <div>
-                <p>{this.state.txt}</p>
-                <Widget update= {this.update.bind(this)} />
-                <Widget update= {this.update.bind(this)} /> 
-                <Widget update= {this.update.bind(this)} />
-            </div>
-        )
-    };
 }
 
-const Widget = (props) =>
-    <input type="text" onChange={props.update} />
+class Heart extends React.Component{
+    render(){
+        return <span>&hearts;</span>
+    }
+}
+const Button = (props) => <button>{props.children}</button>
+
+//     constructor(){
+//         super();
+//         this.state = {
+//             txt: "this is the value from app.js"
+//         }
+//     }
+//     update( e){
+//         this.setState({txt: e.target.value});
+//     }
+//     render(){
+//         return (
+//             <div>
+//                 <p>{this.state.txt}</p>
+//                 <Widget update= {this.update.bind(this)} />
+//             </div>
+//         )
+//     };
+// }
+
+// const Widget = (props) =>
+//     <input type="text" onChange={props.update} />
 
 // App.propTypes ={
 //     txt: React.PropTypes.string,
