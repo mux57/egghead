4317e92003fd9b1eaefabd8e46807ce8ec9899f2
=mux57 <mukesh.gupta@knolskape.com>
2017-09-03 23:40:20 +0530

    custom validation ep 8

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import React from 'react';
 class App extends React.Component{
-
     render() {
-        return <Button> yo <Heart /> </Button>
+        return <Title />
+    }
+}
+const Title= (props) => <h1>Title: {props.text}</h1>
+Title.propTypes = {
+    text(props,propName,component){
+        if (!(propName in props)){
+            return new Error('missing $(propName)')
+        }
+
+        if (props[propName].length < 10){
+            return new Error(" $(propName) length is less than 10")
+        }
     }
 }
 
-class Heart extends React.Component{
-    render(){
-        return <span>&hearts;</span>
-    }
-}
-const Button = (props) => <button>{props.children}</button>
+// class Heart extends React.Component{
+//     render(){
+//         return <span>&hearts;</span>
+//     }
+// }
+// const Button = (props) => <button>{props.children}</button>
 
 //     constructor(){
 //         super();
