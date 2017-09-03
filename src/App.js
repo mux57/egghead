a1b42579ca0c25155bc57abebaa32b9fe196ca02
=mux57 <mukesh.gupta@knolskape.com>
2017-09-03 21:44:36 +0530

    props, propsType,defaultProps

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import React from 'react';
 class App extends React.Component{
     render(){
         // return <h1> hello Eggos</h1>
+        let txt= this.props.txt;
         return (
             <div>
-                <p>hello Eggos</p>
+                <p>{txt}</p>
             </div>
         )
     };
+}
+
+App.propTypes ={
+    txt: React.PropTypes.string,
+    cat: React.PropTypes.number.isRequired
+}
+App.defaultProps ={
+    txt: "yo! I am default txt",
+    cat : 5
 }
 
 // const App = ()=> <h1>Good Morning</h1>
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 {/*import registerServiceWorker from './registerServiceWorker';*/}
 
 ReactDOM.render(
-    <App />,
+    <App txt="I m coming from index.js"/>,
     document.getElementById('root')
 );
 { /*registerServiceWorker();*/ }
