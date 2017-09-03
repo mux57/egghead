e6a375b45fb42406958f3162e6f1b4ff8547a731
=mux57 <mukesh.gupta@knolskape.com>
2017-09-03 21:30:22 +0530

    3rd episode

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 class App extends React.Component{
     render(){
-        return <h1> hello Eggos</h1>
-    }
+        // return <h1> hello Eggos</h1>
+        return (
+            <div>
+                <p>hello Eggos</p>
+            </div>
+        )
+    };
 }
 
 // const App = ()=> <h1>Good Morning</h1>
